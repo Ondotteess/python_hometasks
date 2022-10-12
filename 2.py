f6a3ab4593dac94afc05ecae0343b1d74eb528c2
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2022-10-12 14:49:40 +0700

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-arr2 = ['rtbtr', 'rbtr', 4123, True, '32cc3e']
-arr3 = []
-
-for i in range(min(len(arr1), len(arr2))):
-    arr3.append((arr1[i], arr2[i]))
-
-print(arr3)
