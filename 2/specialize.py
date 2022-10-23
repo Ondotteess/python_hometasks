a201a2e92e3bed19dcdde6dd43ae338f7258d33b
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2022-10-23 21:48:29 +0700

    Update specialize.py

--- 2/specialize.py
+++ 2/specialize.py
@@ -1,16 +1,14 @@
-def sumy(*args):
-    res = 0
-    for i in args:
-        res += i
-    return res
+def sumy(x, y):
+    return x + y
 
-def specialize(f1, *args1):
-    def f2(*args2):
-        return f1(*args1, *args2)
+def specialize(f1, *args, **kwargs):
+    def f2(*_args, **_kwargs):
+        return f1(*args, *_args, **kwargs, **_kwargs)
     return f2
 
-plus_one = specialize(sumy, 1)
-just_two = specialize(sumy, 1, 1)
 
-print(plus_one(10))    # 11
-print(just_two())      # 2
+just_two = specialize(sumy, 1, 1)
+plus_one = specialize(sumy, y=1)
+
+print(plus_one(x=10))    # 11
+print(just_two())        # 2
