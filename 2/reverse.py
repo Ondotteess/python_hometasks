4789d4955d4010af21539cad758a73c88a9a290e
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2022-10-23 23:31:54 +0700

    Update reverse.py

--- 2/reverse.py
+++ 2/reverse.py
@@ -3,6 +3,9 @@ b = {}
 
 for value in a.values():
     c = [key for key, _value in a.items() if value == _value]
-    b[value] = tuple(x for x in c)
+    if len(c) == 1:
+        b[value] = c[0]
+    else:
+        b[value] = tuple(x for x in c)
     
-print(b) #{97832: ('Ivanov', 'Kuznecov'), 55521: ('Petrov', 'Matanov'), 99999: ('Baranov', 'Sidorov'), 12345: ('Fihtengolc',)}
+print(b) #    {97832: ('Ivanov', 'Kuznecov'), 55521: ('Petrov', 'Matanov'), 99999: ('Baranov', 'Sidorov'), 12345: 'Fihtengolc'}
