a2833b8958e886b61322916d7f2308bb7a3a9412
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2022-10-23 21:27:22 +0700

    Update reverse.py

--- 2/reverse.py
+++ 2/reverse.py
@@ -5,4 +5,4 @@ for value in a.values():
     c = [key for key, _value in a.items() if value == _value]
     b[value] = tuple(x for x in c)
     
-print(b)
+print(b) #{97832: ('Ivanov', 'Kuznecov'), 55521: ('Petrov', 'Matanov'), 99999: ('Baranov', 'Sidorov'), 12345: ('Fihtengolc',)}
