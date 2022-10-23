31d2e25223308a01acd6ee068016283f76e201a2
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2022-10-23 21:26:07 +0700

    Update reverse.py

--- 2/reverse.py
+++ 2/reverse.py
@@ -1,13 +1,8 @@
 a = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Baranov": 99999, "Sidorov": 99999, "Fihtengolc": 12345, "Matanov": 55521}
 b = {}
-c = set()
 
-for s in a.items():
-    if s[1] in c:
-        b[s[1]] = tuple(x for x in a.keys() if a[x] == s[1])
-    else:
-        b[s[1]] = s[0]
-
-    c.add(s[1])
-
-print(b) # {97832: ('Ivanov', 'Kuznecov'), 55521: ('Petrov', 'Matanov'), 99999: ('Baranov', 'Sidorov'), 12345: 'Fihtengolc'}
+for value in a.values():
+    c = [key for key, _value in a.items() if value == _value]
+    b[value] = tuple(x for x in c)
+    
+print(b)
