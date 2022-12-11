537d80739e8d0ce7910d2aae1316b66ad9d56b0f
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2022-12-11 22:53:09 +0700

    Update singleton.py

--- 7/singleton.py
+++ 7/singleton.py
@@ -1,1 +1,23 @@
-pass
+class Counter:
+    def __init__(self, initial_count=0, step=1):
+        self.count = initial_count
+        self.step = step
+    def increment(self):
+        self.count += self.step
+
+class Singleton:
+    _instance = None
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+class SingleCounter(Singleton, Counter):
+    pass
+
+c1 = SingleCounter()
+c2 = SingleCounter()
+c3 = Counter()
+
+print(c1 == c2 == c3)
+print(c1 == c2)
