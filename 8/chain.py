b7be96499d654877d239bbd633ae7ccdab2fe17e
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2022-12-12 21:59:25 +0700

    Update chain.py

--- 8/chain.py
+++ 8/chain.py
@@ -2,11 +2,9 @@ def chain(*args):
     res = []
     for x in args:
         res += x
-
     for x in res:
         yield x
 
 it = range(3, 0, -1)
 
-print(chain([1, 2, 3], it ,['a', 'b', 'c']))
-
+print(list(chain([1, 2, 3], it ,['a', 'b', 'c'])))      #   [1, 2, 3, 3, 2, 1, 'a', 'b', 'c']
